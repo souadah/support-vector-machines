92fb88efda4be4c1a2093170883e9865dca4724b
Sarah <souadah@gmail.com>
2016-04-06 11:06:55 -0400

    Find best C and sigma values for SVM

--- dataset3Params.m
+++ dataset3Params.m
@@ -23,11 +23,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+vals = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10, 30];
+pred_err = zeros(length(vals));
 
+for C = vals
+    for sigma = vals
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+        predictions = svmPredict(model, Xval);
+        pred_err(find(vals == C),find(vals == sigma)) = mean(double(predictions ~= yval));
+    end
+end
 
-
-
-
+best_soln = min(pred_err(:));
+[C_ind, sigma_ind] = find(pred_err == best_soln);
+C = vals(C_ind);
+sigma = vals(sigma_ind);
 
 % =========================================================================
 
